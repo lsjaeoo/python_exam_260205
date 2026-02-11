bdbfa7c67eaf04cd390ee36a3107cad842884ecd
lsjaeoo <leesungjae723@gmail.com>
2026-02-12 01:34:55 +0900

    사용자정의 모듈

---
+++ ex1.py
@@ -0,0 +1,12 @@
+def func1():
+   return "모듈에 있는 첫번째 함수"
+def func1():
+   return "모듈에 있는 첫번째 함수"
+def func2():
+   return "모듈에 있는 두번째 함수"
+def func3():
+   return "모듈에 있는 세번째 함수"
+def func4():
+   return "모듈에 있는 네번째 함수"
+def func5():
+   return "모듈에 있는 다섯번째 함수"
